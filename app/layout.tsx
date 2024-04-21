31657bfb5d4495f714c0229adee12766c573acc2
cheesestringhustler <manuel8ld@gmail.com>
2024-04-22 01:20:39 +0200

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import './global.css';
@@ -26,6 +27,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
