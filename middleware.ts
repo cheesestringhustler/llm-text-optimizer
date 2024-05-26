f89a26a2f599ad29a283e2c272510b89c49710b1
cheesestringhustler <manuel8ld@gmail.com>
2024-05-26 23:59:56 +0200

    compose prod

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const allowedOrigins = isDevelopment ? ['http://localhost:3000', process.env.HOST_ADDRESS] : [process.env.HOST_ADDRESS];
+const allowedOrigins = isDevelopment ? ['http://localhost:3000', process.env.HOST_ADDRESS] : ["https://" + process.env.HOST_ADDRESS_PROD];
 
 export function middleware(req: NextRequest) {
     const origin = req.headers.get('origin') || ''; // Default to empty string if null
